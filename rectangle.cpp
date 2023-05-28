04d4d7791be218b243f08d5646092ee30cbb7042
Jayesh Bhadane <134684685+JayeshBadhane@users.noreply.github.com>
2023-05-28 14:09:32 +0530

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,40 +1,42 @@
-#include<iostream>
-namespace std;
+#include <iostream>
+using namespace std;
 int main()
-{
-  int choice 
-  float length,width;
-  do{
-  cout<<"Enter length and width calculat area and perimeter";
-  cin>>length>>width;
-  cout<<"Enter your choice \n 1.For to Calculat Area of Rectanle\n 2.For to Clculat Primeter of Rectangle ";
-  cin>>choice;
-  do
-  {
-     if(cloice==1)
-     {
-       double area;
-       area= length*width;
-       cout<<"Area of Rectangle is: "<<area<<endl;
-    }
-    else
+{   
+    int v, ch;
+    float length, width;
+    do
     {
-      double peri;
-      peri=2(length+width);
-      cout<<"Perimeter of Rectangle is: "<<pari<<endl;
-    }
-    char ch;
-    cout<<"Do you want to continue if yes Enter y or Y";
-    cin>>ch;
-  }
-  while(ch==y/Y)
+    cout << "Enter length and width calculat area and perimeter";
+    cin >> length >> width;
+
+    do
+    {
+        int choice;
+        
+        cout << "Enter your choice \n 1.For to Calculat Area of Rectanle\n 2.For to Clculat Primeter of Rectangle ";
+        cin >> choice;
+        if (choice == 1)
+        {
+            double area;
+            area = length * width;
+            cout << "Area of Rectangle is: " << area << endl;
+        }
+        else
+        {
+            double peri;
+            peri = 2 * (length + width);
+            cout << "Perimeter of Rectangle is: " << peri << endl;
+        }
+
+        cout << "Do you want to continue if yes Enter 1";
+        cin >> ch;
+    } while (ch ==1 );
     cout<<endl;
-     char v;
-    cout<<"Do you want to continue if yes Enter y or Y";
-    cin>>v;
-  }
-   while(v==y/Y)
-    cout<<endl;
-  
+
+       cout << endl;  cout << "Do you want to continue if yes Enter 1";
+        cin >> v;
+    } while (v == 1);
+    cout << endl;
+
     return 0;
 }
