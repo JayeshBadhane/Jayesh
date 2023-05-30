58d0d837097e3ea953c084b1fe1812f1e125eaa1
Jayesh Bhadane <134684685+JayeshBadhane@users.noreply.github.com>
2023-05-30 13:17:35 +0530

    Update day4_ascii.cpp

--- day4_ascii.cpp
+++ day4_ascii.cpp
@@ -6,11 +6,13 @@ using namespace std;
 // Driver code
 int main()
 {
-    char c;
-    cout<<"Enter Charector to Calculet ASCII value and size of ASCII value & size of Charactor";
+     char c;
+    cout<<"Enter Charector to Calculet ASCII value and size of ASCII value & size of Charactor ";
     cin>>c;
     cout << "The ASCII value of " <<c << " is " << int(c)<<endl;
     cout<<"Size of char ("<<c<<") is: "<<sizeof(c)<<endl;
     cout<<"Size of int("<<c<<") is: "<<sizeof(int(c))<<endl;
+    cout<<"Size of float("<<c<<") is: "<<sizeof(float(c))<<endl;
+    cout<<"Size of double("<<c<<") is: "<<sizeof(double(c))<<endl;
     return 0;
 }
