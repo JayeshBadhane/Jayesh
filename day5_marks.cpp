0fdd8c27c2c26ea0b46cc6ab2a721acde77c5ac3
Jayesh Bhadane <134684685+JayeshBadhane@users.noreply.github.com>
2023-06-01 13:39:32 +0530

    Update day5_marks.cpp

--- day5_marks.cpp
+++ day5_marks.cpp
@@ -7,6 +7,10 @@ int main()
     char name[30];
     float avr,per, total=0;
     int marks,n;
+    cout<<"Enter your name ";
+    cin>>name;
+    cout<<"Enter your Roll number ";
+    cin>>rollno;
     cout<<"Enter the Subject Count for Calculating Persentage ";
     cin>>n;
     cout<<"Enter "<<n<<" Subject Marks for Calculating Percentage: ";
@@ -21,6 +25,6 @@ int main()
     per=(total*100)/(n*100);
     cout<<"Your Total Marks is: "<<total<<endl;
     cout<<"Your Average Marks is: "<<avr<<endl;
-    cout<<"Your Total Percentage is: "<<per<<endl;
+    cout<<"Your Total Percentage is: "<<per<<"%"<<endl;
 return 0;
 }
