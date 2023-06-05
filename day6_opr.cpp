c70a9ee388c1206413352d806b4b948c6202fef1
Jayesh Bhadane <134684685+JayeshBadhane@users.noreply.github.com>
2023-06-05 12:52:35 +0530

    Add files via upload

---
+++ day6_opr.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int x,y;
+cout<<"Enter the value of X & Y for perform operations: ";
+cin>>x,y;
+cout<<"Your x is greater than y: "<<(x>y)<<endl;
+cout<<"Your x is less than y: "<<(x<y)<<endl;
+cout<<"Your x is equal to y: "<<(x==y)<<endl;
+return 0;
+}
